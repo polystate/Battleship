4f7ee86a6100c6ef47d01da9dc4416404053053c
unknown <dockadams333@gmail.com>
2023-07-10 19:48:18 +0700

    Initial setup/config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      // Other rules...
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
